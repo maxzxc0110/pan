4c76e7306c8c86a5203512ba50b1dcd1d0168456
int86 <273206894@qq.com>
2020-09-27 17:10:25 +0800

    基于redis实现分块上传

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module bb-pan
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	github.com/garyburd/redigo v1.6.2 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+)
--- src/main.go
+++ src/main.go
@@ -18,6 +18,25 @@ func main(){
 	 http.HandleFunc("/user/signup",handler.SignupHandler)
 	 http.HandleFunc("/user/signin",handler.SignInHandler)
 	 http.HandleFunc("/user/info",handler.HTTPInterceptor( handler.UserInfoHandler))
+
+
+	// 秒传接口
+	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(
+		handler.TryFastUploadHandler))
+
+	
+
+	// 分块上传接口
+	http.HandleFunc("/file/mpupload/init",
+		handler.HTTPInterceptor(handler.InitialMultipartUploadHandler))
+	http.HandleFunc("/file/mpupload/uppart",
+		handler.HTTPInterceptor(handler.UploadPartHandler))
+	http.HandleFunc("/file/mpupload/complete",
+		handler.HTTPInterceptor(handler.CompleteUploadHandler))
+
+
+
+
 	 err := http.ListenAndServe(":8080",nil)
 	 if err != nil{
 	 	fmt.Printf("Failed to start server,err:%s",err.Error())
