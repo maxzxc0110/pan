f9cbbda749126a1d1a50131571fd95b710c22872
int86 <273206894@qq.com>
2020-09-27 16:12:54 +0800

    基于哈希实现秒传

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -9,7 +9,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
+
+	dblayer "bb-pan/src/db"
 )
 
 func UploadHandler(w http.ResponseWriter,r *http.Request){
@@ -58,8 +61,21 @@ func UploadHandler(w http.ResponseWriter,r *http.Request){
 
 		_ = meta.UpdateFileMetaDB(fileMeta)
 
-		//上传成功跳转
-		http.Redirect(w,r,"/file/upload/suc",http.StatusFound)
+
+		r.ParseForm()
+
+		username := r.Form.Get("username")
+
+		suc := dblayer.OnUserFileUploadFinished(username,fileMeta.FileSha1,fileMeta.FileName,fileMeta.FileSize)
+
+		if suc{
+			//上传成功跳转
+			http.Redirect(w,r,"/file/upload/suc",http.StatusFound)
+		}else{
+			w.Write([]byte("Upload Filed"))
+		}
+
+
 	}
 }
 
@@ -169,7 +185,74 @@ func FileDelHandler(w http.ResponseWriter, r *http.Request){
 }
 
 
+// FileQueryHandler : 查询批量的文件元信息
+func FileQueryHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
 
+	limitCnt, _ := strconv.Atoi(r.Form.Get("limit"))
+	username := r.Form.Get("username")
+	//fileMetas, _ := meta.GetLastFileMetasDB(limitCnt)
+	userFiles, err := dblayer.QueryUserFileMetas(username, limitCnt)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(userFiles)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+
+// TryFastUploadHandler : 尝试秒传接口
+func TryFastUploadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	// 1. 解析请求参数
+	username := r.Form.Get("username")
+	filehash := r.Form.Get("filehash")
+	filename := r.Form.Get("filename")
+	filesize, _ := strconv.Atoi(r.Form.Get("filesize"))
+
+	// 2. 从文件表中查询相同hash的文件记录
+	fileMeta, err := meta.GetFileMetaDB(filehash)
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// 3. 查不到记录则返回秒传失败
+	if fileMeta == nil {
+		resp := util.RespMsg{
+			Code: -1,
+			Msg:  "秒传失败，请访问普通上传接口",
+		}
+		w.Write(resp.JSONBytes())
+		return
+	}
+
+	// 4. 上传过则将文件信息写入用户文件表， 返回成功
+	suc := dblayer.OnUserFileUploadFinished(
+		username, filehash, filename, int64(filesize))
+	if suc {
+		resp := util.RespMsg{
+			Code: 0,
+			Msg:  "秒传成功",
+		}
+		w.Write(resp.JSONBytes())
+		return
+	}
+	resp := util.RespMsg{
+		Code: -2,
+		Msg:  "秒传失败，请稍后重试",
+	}
+	w.Write(resp.JSONBytes())
+	return
+}
 
 
 
